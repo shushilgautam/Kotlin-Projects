0e573bc027893a48339181eaf3d643f469cce52b
Shushil Gautam <mr.kaizectric@gmail.com>
2024-06-13 17:09:15 +0545

    login and Registration Kotlin Complete

--- NotesAppWithFirebase/app/src/main/java/com/example/notesappwithfirebase/MainActivity.kt
+++ NotesAppWithFirebase/app/src/main/java/com/example/notesappwithfirebase/MainActivity.kt
@@ -1,20 +1,44 @@
 package com.example.notesappwithfirebase
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.notesappwithfirebase.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        auth=FirebaseAuth.getInstance()
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        binding.intentRegister.setOnClickListener(View.OnClickListener {
+            startActivity(Intent(this,RegistrationActivity::class.java))
+        })
+        binding.login.setOnClickListener(View.OnClickListener {
+            val email = binding.email.text.toString()
+            val password =binding.password.text.toString()
+            if (email.isNotEmpty() and password.isNotEmpty()){
+                auth.signInWithEmailAndPassword(email,password).addOnCompleteListener {
+                    if (it.isComplete){
+                        Toast.makeText(this, "Login Succesfull ",Toast.LENGTH_SHORT).show()
+                        startActivity(Intent(this,HomeActivity::class.java))
+                    }
+                }
+            }
+        })
     }
 }
--- NotesAppWithFirebase/app/src/main/java/com/example/notesappwithfirebase/RegistrationActivity.kt
+++ NotesAppWithFirebase/app/src/main/java/com/example/notesappwithfirebase/RegistrationActivity.kt
@@ -1,20 +1,48 @@
 package com.example.notesappwithfirebase
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.notesappwithfirebase.databinding.ActivityRegistrationBinding
+import com.google.firebase.auth.FirebaseAuth
+
 
 class RegistrationActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegistrationBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding= ActivityRegistrationBinding.inflate(layoutInflater)
+        auth=FirebaseAuth.getInstance()
         enableEdgeToEdge()
-        setContentView(R.layout.activity_registration)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        binding.register.setOnClickListener(View.OnClickListener {
+            val fullname=binding.fullName.text.toString()
+            val email=binding.email.text.toString()
+            val password =binding.password.text.toString()
+            if (email.isNotEmpty() and password.isNotEmpty()){
+                auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener {
+                    if (it.isComplete) {
+                        Toast.makeText(this, "Registration Succesfull", Toast.LENGTH_SHORT).show()
+                        val i = Intent(this, MainActivity::class.java)
+                        startActivity(i)
+
+                    }else {
+                        Toast.makeText(this, it.exception?.message.toString(), Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                }
+            }
+        })
     }
 }
